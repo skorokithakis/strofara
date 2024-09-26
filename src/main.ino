97528745c84ef9e289974514415b72fabbf262d9
Stavros Korokithakis <hi@stavros.io>
2024-09-27 02:31:15 +0300

    Add DFPlayer

--- src/main.ino
+++ src/main.ino
@@ -1,11 +1,12 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
-#define PIN_SPEAKER 2
 #define TURN_ACCEL 6000   // Something?
 #define TURN_DURATION 12  // Deciseconds.
 
@@ -14,8 +15,10 @@
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
 MPU6050 accelgyro;
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
-//MPU6050 accelgyro(0x68, &Wire1); // <-- use for AD0 low, but 2nd Wire (TWI/I2C) object
+SoftwareSerial softSerial(12, 14);
+
+// https://wiki.dfrobot.com/DFPlayer_Mini_SKU_DFR0299
+DFRobotDFPlayerMini myDFPlayer;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -31,8 +34,6 @@ int16_t gx, gy, gz;
 // for a human.
 //#define OUTPUT_BINARY_ACCELGYRO
 
-unsigned long speakerTime = 0;
-int speakerState = 0; // 0 -> off, 1-> playing, 2 -> start.
 int counter = 0;
 
 void setup() {
@@ -42,6 +43,7 @@ void setup() {
     Fastwire::setup(400, true);
 #endif
     Serial.begin(115200);
+    softSerial.begin(9600);
 
     Serial.println("Initializing I2C devices...");
     accelgyro.initialize();
@@ -49,19 +51,10 @@ void setup() {
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-    pinMode(PIN_SPEAKER, OUTPUT);
-}
-
-void checkSpeaker() {
-    if (speakerState == 2) {
-        digitalWrite(PIN_SPEAKER, HIGH);
-        Serial.println("Speaker on!");
-        speakerState = 1;
-        speakerTime = millis();
-    } else if (((millis() - speakerTime) > 4000) && (speakerState == 1)) {
-        Serial.println("Speaker off.");
-        digitalWrite(PIN_SPEAKER, LOW);
-        speakerState = 0;
+    if (!myDFPlayer.begin(softSerial, /*isACK = */true, /*doReset = */true)) {
+        Serial.println(F("Unable to begin:"));
+        Serial.println(F("1.Please recheck the connection!"));
+        ESP.restart();
     }
 }
 
@@ -106,13 +99,13 @@ void loop() {
     delay(100);
     if (abs(ax) > TURN_ACCEL) {
         if (counter == TURN_DURATION) {
-            // Start the speaker.
-            speakerState = 2;
+            Serial.println("Playing audio...");
+            myDFPlayer.volume(30);
+            myDFPlayer.play(1);
         }
         // Keep incrementing the counter, so we don't keep triggering on a long turn.
         counter++;
     } else {
         counter = 0;
     }
-    checkSpeaker();
 }
